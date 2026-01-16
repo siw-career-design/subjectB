24177f9fb7ec8d9896e20348ccbaf57f6cc34e2a
megakujira <tatuya20050820@icloud.com>
2026-01-16 13:16:07 +0900

    ｊ

--- static/js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".coming-soon-btn").forEach(btn => {
-        btn.addEventListener("click", (e) => {
-            e.preventDefault(); // ← 画面遷移を止める
-            alert("準備中です。実装をお待ちください。");
-        });
-    });
-});
